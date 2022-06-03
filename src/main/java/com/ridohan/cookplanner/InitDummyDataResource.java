3a57f3661d7720803b56aecf0a5f161fb1eb77e1
Redouane Khiter <red.khiter.pro@gmail.com>
2022-06-04 00:49:59 +0200

    Feat : Upgrade Init Dummy to create a recipe

--- src/main/java/com/ridohan/cookplanner/InitDummyDataResource.java
+++ src/main/java/com/ridohan/cookplanner/InitDummyDataResource.java
@@ -2,11 +2,18 @@ package com.ridohan.cookplanner;
 
 import com.ridohan.cookplanner.ingredient.Ingredient;
 import com.ridohan.cookplanner.ingredient.IngredientFamily;
+import com.ridohan.cookplanner.quantity.Quantity;
+import com.ridohan.cookplanner.quantity.QuantityKindEnum;
+import com.ridohan.cookplanner.recipe.Recipe;
+import com.ridohan.cookplanner.recipe.RecipeCompound;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Path("/init")
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,33 +23,55 @@ public class InitDummyDataResource {
 
     @GET
     @Transactional
-    public List<IngredientFamily> hello() {
+    public List<Recipe> init() {
 
-        Ingredient apple  = findOrCreateIngredient("Apple");
-        Ingredient orange  = findOrCreateIngredient("Orange");
-        Ingredient pear  = findOrCreateIngredient("Pear");
-        Ingredient cherry  = findOrCreateIngredient("Cherry");
+        initIngredientFamily("Fruit", Arrays.asList("Apple", "Pear","Orange"));
+        initRecipe("Fruit Salad", Map.of("Apple",new Quantity(QuantityKindEnum.UNIT,2D), "Pear",new Quantity(QuantityKindEnum.UNIT,1D)));
+
+        return Recipe.listAll();
+
+    }
+
+    private void initRecipe(String recipeName, Map<String,Quantity> ingredients ){
+
+        Recipe recipe = Recipe.findByName(recipeName);
+        if(recipe == null){
+            recipe = new Recipe();
+            recipe.name = recipeName;
+//            ingredients.forEach((k, v) -> {
+//
+//            });
+
+            for (Map.Entry<String, Quantity> entry : ingredients.entrySet()) {
+                RecipeCompound recipeCompound = new RecipeCompound();
+                recipeCompound.ingredient = findOrCreateIngredient(entry.getKey() );
+                recipeCompound.quantity = entry.getValue();
+                recipe.recipeCompounds.add(recipeCompound);
+            }
 
 
-        IngredientFamily fruitFamily  = IngredientFamily.findByName("Fruit");
-        if(fruitFamily == null){
-            fruitFamily = new IngredientFamily();
-            fruitFamily.name = "Fruit";
-            fruitFamily.ingredients.add(apple);
-            fruitFamily.ingredients.add(orange);
-            fruitFamily.ingredients.add(pear);
-            fruitFamily.ingredients.add(cherry);
-            apple.family=fruitFamily;
-            orange.family=fruitFamily;
-            pear.family=fruitFamily;
-            cherry.family=fruitFamily;
+            recipe.persistAndFlush();
 
-            fruitFamily.persistAndFlush();
         }
 
+    }
 
-        return IngredientFamily.listAll();
 
+        private void initIngredientFamily(String ingredientFamilyName, List<String> ingredients ){
+        IngredientFamily ingredientFamily  = IngredientFamily.findByName(ingredientFamilyName);
+        if(ingredientFamily == null){
+            ingredientFamily = new IngredientFamily();
+            ingredientFamily.name = ingredientFamilyName;
+
+            for(String ingredientString : ingredients){
+                Ingredient ingredient  = findOrCreateIngredient(ingredientString);
+                ingredientFamily.ingredients.add(ingredient);
+                ingredient.family=ingredientFamily;
+
+            }
+
+            ingredientFamily.persistAndFlush();
+        }
     }
 
     public Ingredient findOrCreateIngredient(String name){
