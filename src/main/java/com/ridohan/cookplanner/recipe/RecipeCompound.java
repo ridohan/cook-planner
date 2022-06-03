67a08fb89b3f8605e6cde035d0cea3ef6b5e8a54
Redouane Khiter <red.khiter.pro@gmail.com>
2022-06-03 13:15:14 +0200

    Feat :
    Upgrade to latest Quarkus Version

--- src/main/java/com/ridohan/cookplanner/recipe/RecipeCompound.java
+++ src/main/java/com/ridohan/cookplanner/recipe/RecipeCompound.java
@@ -4,6 +4,7 @@ import com.ridohan.cookplanner.ingredient.Ingredient;
 import com.ridohan.cookplanner.quantity.Quantity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
@@ -12,5 +13,10 @@ public class RecipeCompound extends PanacheEntity {
 
     @ManyToOne
     public Ingredient ingredient;
+    @Embedded
     public Quantity quantity;
+
+    public Quantity getQuantity() {
+        return quantity;
+    }
 }
